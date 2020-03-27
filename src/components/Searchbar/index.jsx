8c64c261b6f4a1c7d0041dabd8b8b12d84f70ddd
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-27 13:17:29 +0100

    files

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import "./index.scss";
-//import axios from "axios";
-
-//https://api.themoviedb.org/3/movie/76341?api_key=6ba659c4bce1a142960639ba1731e656
 
 export const Searchbar = props => {
   const history = useHistory();
--- src/routes/Search/index.jsx
+++ src/routes/Search/index.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQueryParam, NumberParam, StringParam } from "use-query-params";
+
+//import axios from "axios";
+
+//https://api.themoviedb.org/3/movie/76341?api_key=6ba659c4bce1a142960639ba1731e656
 
 export const Search = props => {
+  useEffect(() => {
+    //console.log(props);
+  });
+
   return (
     <main className='content discover'>
       <div className='container'>
