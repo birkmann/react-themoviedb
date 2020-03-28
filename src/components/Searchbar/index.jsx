ef5f18e4e9c2ad831cc676e3cd249b01651f3dc9
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-28 01:43:39 +0100

    files

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useQueryParam, StringParam } from "use-query-params";
+import axios from "axios";
+
 import "./index.scss";
 
 export const Searchbar = props => {
+  const [search] = useQueryParam("query", StringParam);
+
+  const [searchValue, setSearchValue] = useState(search);
+
+  const handleChange = event => {
+    setSearchValue(event.target.value);
+    axios({
+      method: "get",
+      url:
+        "https://api.themoviedb.org/3/search/movie?api_key=6ba659c4bce1a142960639ba1731e656&language=en-US&query=" +
+        searchValue +
+        "&page=1&include_adult=false"
+    }).then(response => {
+      console.log(response.data);
+    });
+  };
+
   const history = useHistory();
   const handleSubmit = event => {
     event.preventDefault();
     history.push("/search/?query=" + event.target.query.value);
   };
   return (
-    <div className='form-wrapper'>
-      <div className='container'>
+    <div className="form-wrapper">
+      <div className="container">
         <form onSubmit={handleSubmit}>
-          <i className='material-icons icon-search'>search</i>
+          <i className="material-icons icon-search">search</i>
           <input
-            type='text'
-            name='query'
-            placeholder='Search for a movie, tv show, person...'
+            type="text"
+            name="query"
+            value={searchValue}
+            onChange={handleChange}
+            placeholder="Search for a movie, tv show, person..."
           />
         </form>
       </div>
--- src/routes/Search/index.jsx
+++ src/routes/Search/index.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from "react";
-import { useQueryParam, NumberParam, StringParam } from "use-query-params";
-
-//import axios from "axios";
-
-//https://api.themoviedb.org/3/movie/76341?api_key=6ba659c4bce1a142960639ba1731e656
+import React, { useState, useEffect } from "react";
+import { useQueryParam, StringParam } from "use-query-params";
+import axios from "axios";
 
 export const Search = props => {
+  const [search] = useQueryParam("query", StringParam);
   useEffect(() => {
-    //console.log(props);
+    axios({
+      method: "get",
+      url:
+        "https://api.themoviedb.org/3/search/movie?api_key=6ba659c4bce1a142960639ba1731e656&language=en-US&query=" +
+        search +
+        "&page=1&include_adult=false"
+    }).then(response => {
+      console.log(response.data);
+    });
   });
 
   return (
-    <main className='content discover'>
-      <div className='container'>
-        <h2>Search</h2>
+    <main className="content discover">
+      <div className="container">
+        <h2>Search:</h2>
       </div>
     </main>
   );
